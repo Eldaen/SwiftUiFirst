767f019cb5ce40dde0afc815b7cd6fa53f6c8882
Denis Sizov <sv_skatch@bk.ru>
2021-07-21 14:58:45 +0300

    Initial Commit

---
+++ MyFirstProject/MyFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstProjectApp.swift
+//  MyFirstProject
+//
+//  Created by Денис Сизов on 21.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
