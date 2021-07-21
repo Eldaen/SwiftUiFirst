253882cd0fb664268e36a74e63ac262a7a061549
Denis Sizov <sv_skatch@bk.ru>
2021-07-21 16:27:08 +0300

    First swift UI test

--- MyFirstProject/ContentView.swift
+++ MyFirstProject/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        VStack(alignment: .center, spacing: 10) { // Ещё бывает HStack для горизонтального размещения
+//            Text("Hello, Swift!")
+//                .padding(.horizontal, 50)
+//            Spacer() // Если не задана величина, то занимает всё пространство
+//                .frame(height: 50)
+//            Text("Hello")
+//            Divider()
+//            Text("Hello, Sw")
+//        }
+        
+        ZStack { // Нужен чтобы размещать вьюхи в плоскости Z, т.е. накладывать их
+            Image("epic")
+                .scaleEffect(0.3)
+            Text("Specialized Epic 2018")
+                .padding(.top, 400)
+                .font(.title)
+        }
+        //Стаки можно помещать в стаки
     }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
